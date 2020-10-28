efe6c320fd11c7ecf0f7a30cb34cf601a99de64a
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-10-28 13:06:13 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TODO List Flutter Application
+ 
---
+++ android/app/src/main/java/com/hadi/to_do_list/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hadi.to_do_list;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
